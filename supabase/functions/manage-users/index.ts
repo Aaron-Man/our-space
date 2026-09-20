8359490b56c8fa1e18d954fae234df3f386d4304
19169015383 <952768018@qq.com>
2026-09-20 15:23:37 +0800

    chore: 更新 Edge Function 注释触发重新部署

--- supabase/functions/manage-users/index.ts
+++ supabase/functions/manage-users/index.ts
@@ -1,5 +1,6 @@
 // supabase/functions/manage-users/index.ts
 // Edge Function for secure user management
+// Last updated: 2026-09-20 - Added display_name optimization
 
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
